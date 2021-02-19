cd63ae9229d098f955481b0250d8e02bf9316ecf
Luis Mota <lsmm0001@ce.pucmm.edu.do>
2021-02-19 15:55:59 -0400

    Luke's room instruction

--- Assets/Script/UI/HUD.cs
+++ Assets/Script/UI/HUD.cs
@@ -6,6 +6,7 @@ public class HUD : MonoBehaviour
 {
     public GameObject player;
     public GameObject first;
+    public GameObject third;
     public Transform position;
 
     private float initial_position;
@@ -17,21 +18,41 @@ public class HUD : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        instantiatedObject = Instantiate(first,position.position, Quaternion.identity, GameObject.FindGameObjectWithTag("HUD").transform) as GameObject;
-        initial_position = player.transform.position.x;
+        if(third == null)
+        {
+            instantiatedObject = Instantiate(first, position.position, Quaternion.identity, GameObject.FindGameObjectWithTag("HUD").transform) as GameObject;
+            initial_position = player.transform.position.x;
+        }
+        else
+        {
+            if
+            instantiatedObject = Instantiate(third, position.position, Quaternion.identity, GameObject.FindGameObjectWithTag("HUD").transform) as GameObject;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs((player.transform.position.x - initial_position)) > 0.05)
+        if(third == null)
         {
-            if (instantiatedObject)
+            if (Mathf.Abs((player.transform.position.x - initial_position)) > 0.05)
             {
-                instantiatedObject.GetComponent<Animator>().SetTrigger("Vanish");
+                if (instantiatedObject)
+                {
+                    instantiatedObject.GetComponent<Animator>().SetTrigger("Vanish");
+                }
+                Invoke("DestroyO", 1);
             }
-            Invoke("DestroyO", 1);
         }
+        else
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                Invoke("DestroyO", 1);
+            }
+        }
+
     }
     void DestroyO()
     {
