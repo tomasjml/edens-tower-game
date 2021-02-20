13f8824c921577fd6e2c358c100a7cc7cc92dc16
Diana <diana2monegro1@gmail.com>
2021-02-20 17:27:31 -0400

    Adding picking up sword logic without animations

--- Assets/Script/Player/FindingTiaraAndSword.cs
+++ Assets/Script/Player/FindingTiaraAndSword.cs
@@ -11,17 +11,23 @@ public class FindingTiaraAndSword : MonoBehaviour
     private PlayerControllerV2 playerMainScript;
     public Transform textPlayerBox;
     public Text dialogPlayerText;
+    public Transform sword;
+    
+    public Transform textPlayerBoxSword;
+    public Text dialogPlayerTextSword;
+    
     void Start()
     {
         playerMainScript=gameObject.GetComponent<PlayerControllerV2>();
         veces=0;
         textPlayerBox.gameObject.SetActive(false);
+        textPlayerBoxSword.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(veces==0 && transform.position.x>1.16 &&tiara.gameObject.activeSelf==true){   
+        if(veces==0 && transform.position.x>1.093 &&tiara.gameObject.activeSelf==true){   
             playerMainScript.enableKeys(false);
             textPlayerBox.gameObject.SetActive(true);
             dialogPlayerText.text="That's Lisa's Tiara! I'll give it to her when i rescue her. I need to find a weapon.";
@@ -31,7 +37,26 @@ public class FindingTiaraAndSword : MonoBehaviour
             playerMainScript.enableKeys(true);
             textPlayerBox.gameObject.SetActive(false);
             dialogPlayerText.text="";
-            
+            veces++;
+        }
+        if(Input.GetKeyDown(KeyCode.W) && veces==2){
+            tiara.gameObject.SetActive(false);
+        }
+
+        if(veces==2 && transform.position.x>1.44 &&sword.gameObject.activeSelf==true){   
+            playerMainScript.enableKeys(false);
+            textPlayerBoxSword.gameObject.SetActive(true);
+            dialogPlayerTextSword.text="This sword is the weapon i need!";
+            veces++;
+        }
+        if(Input.GetKeyDown(KeyCode.W) && veces==3){
+            playerMainScript.enableKeys(true);
+            textPlayerBoxSword.gameObject.SetActive(false);
+            dialogPlayerTextSword.text="";
+            veces++;
+        }
+        if(Input.GetKeyDown(KeyCode.W) && veces==4){
+            sword.gameObject.SetActive(false);
         }
     }
 }
