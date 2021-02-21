7ec6b870902e43e1a0929802c27c6792a7a4861c
Luis Mota <lsmm0001@ce.pucmm.edu.do>
2021-02-21 19:14:09 -0400

    Adding task menu for all scenes and fixing dialog

--- Assets/Script/UI/TaskLoader.cs
+++ Assets/Script/UI/TaskLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class TaskLoader : MonoBehaviour
@@ -11,17 +12,29 @@ public class TaskLoader : MonoBehaviour
     private GameObject instantiatedObject0 = null;
     private GameObject instantiatedObject1 = null;
     private GameObject instantiatedObject2 = null;
+    private string scene;
+    private bool complete = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scene = SceneManager.GetActiveScene().name;
+        if (scene.Equals("Dormitorio Luke"))
+        {
+            complete = true;
+        }
+        if (scene.Equals("Dormitorio Luke") || scene.Equals("Pasillo"))
+        {
+            instantiatedObject0 = Instantiate(tasks[0], position0.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Task").transform) as GameObject;
+            instantiatedObject1 = Instantiate(tasks[1], position1.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Task").transform) as GameObject;
+            instantiatedObject2 = Instantiate(tasks[2], position2.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Task").transform) as GameObject;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -34,7 +47,23 @@ public class TaskLoader : MonoBehaviour
                 instantiatedObject1 = Instantiate(tasks[1], position1.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Task").transform) as GameObject;
                 instantiatedObject2 = Instantiate(tasks[2], position2.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Task").transform) as GameObject;
             }
-            
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (!scene.Equals("Dormitorio Luke") && !scene.Equals("Pasillo"))
+        {
+            if (instantiatedObject0)
+            {
+                instantiatedObject1.GetComponent<Animator>().SetBool("Appear", true);
+                instantiatedObject2.GetComponent<Animator>().SetBool("Appear", true);
+                instantiatedObject0.GetComponent<Animator>().SetBool("Appear", true);
+            }
+        }
+        if (complete == true)
+        {
+            instantiatedObject1.GetComponent<Animator>().SetTrigger("Complete");
         }
     }
 }
