1907ee9dfe541be55f02411a144fcca73bc2b960
Miguel J. Rivas <20181219@ce.pucmm.edu.do>
2021-02-21 00:27:19 -0400

    The kid already learned how to attack and follow the player
    The enemy is able to attack the player and follow him. Note: It was made with the prefab that says "Player." If anyone tries to use it with the one that says "Play3r" it is not going to work unless some things are changed.

--- Assets/Script/First Enemy/Enemy.cs
+++ Assets/Script/First Enemy/Enemy.cs
@@ -9,6 +9,11 @@ public class Enemy : MonoBehaviour
     Transform enemyTransform;
     public float speed = 3f;
     private bool facingRight;
+    public float followingDistance = 1f;
+    public float attackingDistance = 0.2f;
+    public bool shouldAttack = false;
+    public float aimingTime = 0.2f;
+    public float attackTime = 1.3f;
 
     private Rigidbody2D _rigidbody;
     Animator _animator;
@@ -38,55 +43,84 @@ public class Enemy : MonoBehaviour
 
         target = GameObject.FindWithTag("Player").transform;
         float distance = target.transform.position.x - transform.position.x;
-        if(distance < 0)
+
+        if(shouldAttack == false)
         {
-            distance = distance * -1f;
-        }
-
-        if (distance < 1f)
-        {
-            Vector3 targetHeading = target.position - transform.position;
-            Vector3 targetDirection = targetHeading.normalized;
-
-            //rotate to look at the player
-
-            if (facingRight == false)
-            {
-                if(target.transform.position.x > transform.position.x)
-                {
-                    flip();
-                }
-            }
-            if (facingRight == true)
-            {
-                if (target.transform.position.x < transform.position.x)
-                {
-                    flip();
-                }
-            }
-
-            float horizontalVelocity = speed;
-
-            if (facingRight == false)
-            {
-                horizontalVelocity = horizontalVelocity * -1f;
-            }
-
-            _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
-
+            moving(distance);
         }
     }
-
-    private void LateUpdate()
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+            if (shouldAttack == false && collision.CompareTag("Player")) //If the enemy is not attacking and the player is in the trigger
+            {
+                StartCoroutine(Attack()); //Attack
+            }
+ 
+    }
+    private void LateUpdate() 
     {
         _animator.SetBool("Idle", _rigidbody.velocity == Vector2.zero);
     }
 
-    public void flip()
+    private void moving(float distance)
+    {
+
+        if (distance < 0) //If the distance is negative
+        {
+            distance = distance * -1f; //We make it postive
+        }
+
+        if (distance < followingDistance)
+        {
+            Vector3 targetHeading = target.position - transform.position;
+            Vector3 targetDirection = targetHeading.normalized;
+
+            //Rotate to look at the player
+
+            if (facingRight == false) //If the enemy is facing left
+            {
+                if (target.transform.position.x > transform.position.x) //And the player is on the right side
+                {
+                    flip();
+                }
+            }
+            if (facingRight == true) //If the enemy is facing right 
+            {
+                if (target.transform.position.x < transform.position.x) //And the player is on the left side
+                {
+                    flip();
+                }
+            }
+
+            float horizontalVelocity = speed; 
+
+            if (facingRight == false) //If the enemy is facing left we have to change the speed to face left
+            {
+                horizontalVelocity = horizontalVelocity * -1f;
+            }
+
+            _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y); //The enemy moves.
+        }
+    }
+    public void flip() //Makes the enemy face the other way 
     {
         facingRight = !facingRight;
         float localScaleX = transform.localScale.x;
         localScaleX = localScaleX * -1f;
         transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private IEnumerator Attack()
+    {
+        float speedBackup = speed;
+        speed = 0f;
+        shouldAttack = true;
+
+        yield return new WaitForSeconds(aimingTime);
+        _animator.SetTrigger("IsAttacking");
+        yield return new WaitForSeconds(attackTime);
+
+        shouldAttack = false;
+        speed = speedBackup;
     }
 }
