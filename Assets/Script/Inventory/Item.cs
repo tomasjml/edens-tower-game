9982555b02e1a85f8aeb86156353ff2fa61e1a72
Tomas Molina <jtml.mass@gmail.com>
2021-02-27 00:25:39 -0400

    Add Item base class

--- Assets/Script/Inventory/Item.cs
+++ Assets/Script/Inventory/Item.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Item
 {
-    // Start is called before the first frame update
-    void Start()
+    public int id;
+    public string title;
+    public string description;
+    public Sprite icon;
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+
+    public Item(int id, string title, string description, Sprite icon, Dictionary<string, int> stats)
     {
-        
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+        this.stats = stats;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Item(Item item)
     {
-        
+        this.id = item.id;
+        this.title = item.title;
+        this.description = item.description;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+        this.stats = item.stats;
     }
+
 }
