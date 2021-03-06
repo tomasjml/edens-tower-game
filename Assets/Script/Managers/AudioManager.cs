2b3f83b541b537df3c43c4219264140409e37b3f
Tomas Molina <jtml.mass@gmail.com>
2021-03-06 18:42:05 -0400

    Adding Audio Manager

---
+++ Assets/Script/Managers/AudioManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null;
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        GetComponent<AudioSource>().Play();
+    }
+}
--- Assets/Script/Managers/Loader.cs
+++ Assets/Script/Managers/Loader.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Loader : MonoBehaviour
 {
     public GameObject gameManager;
+    public GameObject audioManager;
     public SaveAndLoad saveAndLoad;
 
     // Start is called before the first frame update
@@ -14,6 +15,12 @@ public class Loader : MonoBehaviour
         {
             Instantiate(gameManager);
         }
+
+        if (AudioManager.instance == null)
+        {
+            Instantiate(audioManager);
+        }
+
 
         //if(SaveAndLoad.instance == null)
         //{
