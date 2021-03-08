38f67445415be314c174c09ace24109a7c6fe98c
Eliam Pimentel <exps0001@ce.pucmm.edu.do>
2021-03-08 16:53:13 -0400

    Part 1 w enemies (no hacen daño)

---
+++ Assets/Script/Player/HealthPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour
+{
+    public void Hit()
+    {
+        Debug.Log("auch");
+    }
+}
