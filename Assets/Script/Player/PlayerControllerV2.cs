360a6d369cb4bd2c753ff299df9611eaa4e2bde2
Diana <diana2monegro1@gmail.com>
2021-03-09 10:15:30 -0400

    fixing little things

--- Assets/Script/Player/PlayerControllerV2.cs
+++ Assets/Script/Player/PlayerControllerV2.cs
@@ -117,13 +117,13 @@ public class PlayerControllerV2 : MonoBehaviour
             flip();
         }
         //Esta saltando?
-        if(Input.GetButtonDown("Jump") &&  veces==0 && _isGrounded==true&&enableKey==true && !(sceneName.Equals("Sala") || sceneName.Equals("Pasillo"))){
+        if(Input.GetKeyDown(KeyCode.Space) &&  veces==0 && _isGrounded==true&&enableKey==true && !(sceneName.Equals("Sala") || sceneName.Equals("Pasillo"))){
             _body.AddForce(Vector2.up *jumpForce, ForceMode2D.Impulse);
             isJumping=true;
             veces++;
             
         }
-        else if(Input.GetButtonDown("Jump") && veces<jumpsWanted &isJumping==true&&enableKey==true && !(sceneName.Equals("Sala") || sceneName.Equals("Pasillo")))
+        else if(Input.GetKeyDown(KeyCode.Space) && veces<jumpsWanted &isJumping==true&&enableKey==true && !(sceneName.Equals("Sala") || sceneName.Equals("Pasillo")))
         {
             _body.AddForce(Vector2.up *jumpForce, ForceMode2D.Impulse);
            veces++;
