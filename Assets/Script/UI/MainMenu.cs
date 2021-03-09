155e9757492d349fd5a331418b99904df41e37cb
Abraham <20190315@ce.pucmm.edu.do>
2021-03-08 23:32:35 -0400

    Polished log-in screen UI

--- Assets/Script/UI/MainMenu.cs
+++ Assets/Script/UI/MainMenu.cs
@@ -11,7 +11,6 @@ public class MainMenu : MonoBehaviour
     public GameObject LoadMenu;
     public GameObject MenuButtons;
     public GameObject LoginMenu;
-    public GameObject Background;
 
     // Start is called before the first frame update
     void Start()
