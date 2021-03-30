8d0ae5652b8207941c4c3dcb7e708b7d1b0186f1
Diana <diana2monegro1@gmail.com>
2021-03-29 20:53:12 -0400

    testing dialog stuff

--- Assets/Script/Other/dialogueStopPlayer.cs
+++ Assets/Script/Other/dialogueStopPlayer.cs
@@ -5,24 +5,22 @@ using UnityEngine;
 public class dialogueStopPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject dialogue;
-    public GameObject character;
-    private Rigidbody2D rb;
-    private Vector2 movement;
+    public GameObject dialogCloud;
+    private Rigidbody2D _rigidbody;
+    Animator _animator;
     void Start()
     {
-        rb=character.gameObject.GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-         movement = new Vector2(horizontalInput, 0f);
-        if(dialogue.activeSelf==true){
-            Debug.Log("hiiii");
-            movement=Vector3.zero;
-
+        if(dialogCloud!=null &&dialogCloud.activeSelf){
+            Debug.Log("la nube is active hoe");
+            _rigidbody.velocity = Vector3.zero;
+            _animator.SetBool("Idle", _rigidbody.velocity == Vector2.zero);
         }
     }
 }
