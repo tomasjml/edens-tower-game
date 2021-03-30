f8f3e5072165af5039bf28208701d32f9e64673b
Abraham <20190315@ce.pucmm.edu.do>
2021-03-30 15:49:05 -0400

    UI general overhaul

--- Assets/Script/UI/SliderController.cs
+++ Assets/Script/UI/SliderController.cs
@@ -5,19 +5,15 @@ using UnityEngine.UI;
 
 public class SliderController : MonoBehaviour
 {
-    public Text label;
-
     public void increase(float value)
     {
         GetComponent<Slider>().value += value;
         value = GetComponent<Slider>().value * 100;
-        label.text = value.ToString();
     }
 
     public void decrease(float value)
     {
         GetComponent<Slider>().value -= value;
         value = GetComponent<Slider>().value * 100;
-        label.text = value.ToString();
     }
 }
