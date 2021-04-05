a8cbf5c75009c7b00133f4ef9ba0a990d559471a
Eliam Pimentel <exps0001@ce.pucmm.edu.do>
2021-04-05 02:38:14 -0400

    Kaos 3 fases

--- Assets/TwistedON.cs
+++ Assets/TwistedON.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class TwistedON : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Animator _animator;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Player"))
+        {
+            _animator.GetComponentInParent<Animator>().SetTrigger("Twisted");
+        }
     }
 }
