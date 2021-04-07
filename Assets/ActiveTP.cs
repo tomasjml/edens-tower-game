070e302bde0e02e80db8c394ea821316ce2532fe
Eliam Pimentel <exps0001@ce.pucmm.edu.do>
2021-04-07 18:55:41 -0400

    Lisa's room 90%

---
+++ Assets/ActiveTP.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveTP : MonoBehaviour
+{
+    [Header("Enemigo")]
+    public GameObject _Kaos;
+    [Header("TP")]
+    public GameObject _TP;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_Kaos.GetComponent<BossHealth>().getCurrentHealthEnemy() <= 0)
+        {
+            _TP.SetActive(true);
+        }
+    }
+}
