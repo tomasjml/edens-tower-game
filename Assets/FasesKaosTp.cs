f11ac465a477302d325dac73ca0c19ef4fcbda7f
Eliam Pimentel <exps0001@ce.pucmm.edu.do>
2021-04-07 01:33:31 -0400

    DIalog S6

---
+++ Assets/FasesKaosTp.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FasesKaosTp : MonoBehaviour
+{
+    [Header("Kaos")]
+    public GameObject _KaosFase;
+    [Header("Zona TP")]
+    public GameObject _FaseTP;
+    public GameObject _Player;
+    [Header("Cambio de camara")]
+    public GameObject _CameraON;
+    public GameObject _CameraOFF;
+    float time;
+    int cant = 0;
+
+    private void Update()
+    {
+        if (_KaosFase.GetComponent<BossHealth>().getCurrentHealthEnemy() <= 0)
+        {
+            if(cant < 1)
+            {
+                time = getNextTime();
+                cant++;
+            }
+            if (Time.time > time)
+            {
+                _Player.transform.position = _FaseTP.transform.position;
+                _CameraOFF.SetActive(false);
+                _CameraON.SetActive(true);
+
+            }
+        }
+    }
+   /* private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(_KaosFase.GetComponent<BossHealth>().getCurrentHealthEnemy() <= 0 )
+        {
+            if(collision.gameObject.CompareTag("Player"))
+            {
+                time = getNextTime();
+                if(Time.time > time)
+                {
+                    _Player.transform.position = _FaseTP.transform.position;
+                    _CameraOFF.SetActive(false);
+                    _CameraON.SetActive(true);
+                }
+            }
+        }
+    }*/
+
+    float getNextTime()
+    {
+        return Time.time + 5f;
+    }
+}
